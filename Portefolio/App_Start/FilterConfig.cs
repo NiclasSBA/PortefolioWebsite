2a26441a3da2dbf6e6259400cb758152c52debbf
Niclas Andersen <niclas_andersen@hotmail.com>
2018-03-14 21:52:46 +0100

    Add project files.

---
+++ Portefolio/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Portefolio
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
