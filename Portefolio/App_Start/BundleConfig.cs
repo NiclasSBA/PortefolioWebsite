dc3bf8044993f64a47624bd5a436458dea421126
Niclas Andersen <niclas_andersen@hotmail.com>
2018-03-20 22:15:07 +0100

    Started working on the styling from the chosen theme. Edited a little in scripts.js. Index.cshtml is the only one thats currently being worked on.

--- Portefolio/App_Start/BundleConfig.cs
+++ Portefolio/App_Start/BundleConfig.cs
@@ -21,12 +21,27 @@ namespace Portefolio
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/modernizr-2.8.1.min.js",
+                      "~/Scripts/jquery.js",
+                      "~/Scripts/bootstrap.min.js",
+                      "~/Scripts/wow.min.js",
+                      "~/Scripts/hippo-offcanvas.js",
+                      "~/Scripts/jquery.easing.min.js",
+                      "~/Scripts/jquery.magnific-popup.min.js",
+                      "~/Scripts/jquery.shuffle.min.js",
+                      "~/Scripts/scripts.js",
+                      "~/Scripts/jquery.easypiechart.min.js",
+                      "~/Scripts/jquery.easing.min.js"
+                      ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css",
-                      "~/styles/Styles.min.css"));
+                      "~/styles/Styles.min.css",
+                      "~/styles/css/*.css",
+                      "~/css/font-awesome.min.css",
+                      "~/fonts/flaticon/flaticon.css"));
         }
     }
 }
